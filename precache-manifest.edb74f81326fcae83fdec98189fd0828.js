5c2b9a6505b0cf7b355e5e468299c1a79dac9df5
Hyunjin95 <das01063@snu.ac.kr>
2019-05-12 15:01:40 +1000

    Updates

---
+++ precache-manifest.edb74f81326fcae83fdec98189fd0828.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c73417a998e69ffbc4daf78dd22569b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f126f6cbfc50f54e59f",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4141c04694f734079784",
+    "url": "/static/js/2.6303a8f5.chunk.js"
+  },
+  {
+    "revision": "7f126f6cbfc50f54e59f",
+    "url": "/static/js/main.59f6c0dd.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e31fef47d5753d8392e6ee426ad7a19a.js"
+  "/precache-manifest.edb74f81326fcae83fdec98189fd0828.js"
 );
 
 self.addEventListener('message', (event) => {
