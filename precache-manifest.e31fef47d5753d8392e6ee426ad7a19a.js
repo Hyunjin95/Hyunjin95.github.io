9500e208e35690632ea1b670e34bfd9737b56a48
Hyunjin95 <das01063@snu.ac.kr>
2019-05-12 14:29:02 +1000

    Updates

--- precache-manifest.e31fef47d5753d8392e6ee426ad7a19a.js
+++ precache-manifest.e31fef47d5753d8392e6ee426ad7a19a.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3a7051caf9df4cd3aa65306b9f708548",
+    "revision": "8fcfafb128f4783b64442643ba0e40a8",
     "url": "/index.html"
   },
   {
-    "revision": "4b623e8593c9ac990379",
+    "revision": "59a1fbfc23ea47cbc9af",
     "url": "/static/css/main.2cce8147.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.b41502e9.chunk.js"
   },
   {
-    "revision": "4b623e8593c9ac990379",
-    "url": "/static/js/main.28647029.chunk.js"
+    "revision": "59a1fbfc23ea47cbc9af",
+    "url": "/static/js/main.81bf7dc6.chunk.js"
   },
   {
     "revision": "42ac5946195a7306e2a5",
